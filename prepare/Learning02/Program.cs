cca62bea58fb9572e682b8edf77ed3a64889b4b8
kwamena-koomson <kwamenakooms@gmail.com>
2024-04-29 17:13:11 +0100

    completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+  static void Main(string[] args)
+  {
+    Job job1 = new Job();
+    job1._jobTitle = "Software Engineer";
+    job1._company = "Microsoft";
+    job1._startYear = 2500;
+    job1._endYear = 2520;
+
+    Job job2 = new Job();
+    job2._jobTitle = "COO";
+    job2._company = "Spotify";
+    job2._startYear = 2000;
+    job2._endYear = 3000;
+
+    Resume resume = new Resume();
+    resume._name = "Kwamena Koomson";
+    resume._jobs.Add(job1);
+    resume._jobs.Add(job2);
+
+    resume.Display();
+  }
 }
