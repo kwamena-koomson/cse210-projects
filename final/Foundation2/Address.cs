cf11a8b6756258c0e4e2e770f52502280a7fc796
kwamena-koomson <kwamenakooms@gmail.com>
2024-06-01 11:29:46 +0100

    updated

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -2,16 +2,19 @@ using System;
 
 public class Address
 {
+    // Member variables to store address components
     private string _street;
     private string _city;
     private string _stateOrProvince;
     private string _country;
 
+    // Default constructor
     public Address()
     {
 
     }
 
+    // Parameterized constructor to initialize address components
     public Address(string street, string city, string stateOrProvince, string country)
     {
         _street = street;
@@ -20,21 +23,23 @@ public class Address
         _country = country;
     }
 
+    // Method to determine if the address is international based on country
     public bool IsInternational()
     {
         bool isInternational = true;
-        if(_country.ToLower() == "usa")
+        if(_country.ToLower() == "usa") // Check if the country is USA
         {
-            isInternational = false;
+            isInternational = false; // If USA, address is not international
             return isInternational;
         } else {
-            return isInternational;
+            return isInternational; // If not USA, address is international
         }
     }
 
+    // Method to get the complete address as a formatted string
     public string GetAddress()
     {
         string address = $"{_street}, {_city}, {_stateOrProvince}, {_country.ToUpper()}";
-        return address;
+        return address; // Return the formatted address string
     }
 }
--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -2,37 +2,43 @@ using System;
 
 public class Customer
 {
+    // Member variables to store customer name and address
     private string _name;
     private Address _address;
 
+    // Default constructor
     public Customer()
     {
 
     }
 
+    // Parameterized constructor to initialize customer with name
     public Customer(string name)
     {
         _name = name;
     }
 
+    // Method to get the customer's name
     public string DisplayName()
     {
         return _name;
     }
 
+    // Method to set the customer's address
     public void SetAddress(string street, string city, string stateOrProvince, string country)
     {
-        _address = new Address(street, city, stateOrProvince, country);
+        _address = new Address(street, city, stateOrProvince, country); // Create new address object
     }
 
+    // Method to get the formatted address of the customer
     public string DisplayAddress()
     {
-        return _address.GetAddress();
+        return _address.GetAddress(); // Get the address string
     }
 
+    // Method to check if the customer's address is international
     public bool GetInternational()
     {
-        return _address.IsInternational();  
+        return _address.IsInternational(); // Get international status from address object  
     }
-    
 }
